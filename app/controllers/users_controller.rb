c1fa683a2dce524f811b47bad89b71fe3b8b4111
salmotairy <s.musaad@gmail.com>
2018-12-19 21:00:28 +0300

    End point is here

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,21 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  # localhost:3000 => current_user
+  # localhost:3000/users/saud @u
   def show
-    @user = User.find_by(username: params[:id])
-    # If there is a username in the URL, find the user with that username
-    # If you can't a find a user or there are no params (no dynamic stuff in the URL)
-    # set @user to be whoever is logged in
-    if @user.id == current_user.id
+    if params[:id]
+      @user = User.find_by(username: params[:id])
+      if (@user == current_user)
+        @pending_approvals = Approval.where user_two_id: current_user.id, approval: false
+      end
+      # If the user you are looking at is the current_user, set pending_approvals to be the same as line 19
+    else
+      # If there is a username in the URL, find the user with that username
+      # If you can't a find a user or there are no params (no dynamic stuff in the URL)
+      # set @user to be whoever is logged in
       @user = current_user
       @pending_approvals = Approval.where user_two_id: current_user.id, approval: false
-      # raise
     end
   end
 
